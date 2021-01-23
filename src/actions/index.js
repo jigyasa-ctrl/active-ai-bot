4295d5404e473cd40174e9345e09aa2ef35e66db
jigyasa <64299683+jigyasa-ctrl@users.noreply.github.com>
2021-01-23 11:29:32 +0530

    Create index.js

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+export const gallery = () => {
+  return {
+    type: "GALLERY",
+  };
+};
+export const tweets = () => {
+  return {
+    type: "TWEETS",
+  };
+};
+export const text = () => {
+  return {
+    type: "TEXT",
+  };
+};
